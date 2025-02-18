3735f74275899c4e45c879462e8c366a160ba6c5
edwina sweeney <e-sweeney@users.noreply.github.com>
2025-02-18 12:35:37 +0000

    corrected mistake 31,1

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 
 df = read_csv("Salary_Data.csv")
 
-X = df["YearsExperience"].values.reshape(30,1)
+X = df["YearsExperience"].values.reshape(31,1)
 y = df["Salary"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
